8f0ca73c900f9832c56ade25ffeb8091a6c76e9d
shanejix <shanejix@vip.163.com>
2020-01-18 21:25:15 +0800

    add hooks-todo

---
+++ react_hooks/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
+import "todomvc-app-css/index.css";
+
+import Footer from "./components/Footer";
+import TodoList from "./containers/TodoList";
+
+function App() {
+  return (
+    <HashRouter>
+      <React.Fragment>
+        <div className="todoapp">
+          <Route path="/:filter?" component={TodoList} />
+        </div>
+        <Footer />
+      </React.Fragment>
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ react_hooks/src/hooks/useOnEnter.js
@@ -0,0 +1,13 @@
+import { useCallback } from "react";
+
+export default function useOnEnter(callback, inputs) {
+  return useCallback(
+    event => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        callback(event);
+      }
+    },
+    inputs
+  );
+}
